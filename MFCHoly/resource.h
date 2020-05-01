1b99f3e70f4b70b911f9d8d241a1a7a89f03d2e4
holy <holy1017@naver.com>
2020-05-01 17:09:17 +0900

    초기 커밋.

---
+++ MFCHoly/resource.h
@@ -0,0 +1,45 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MFCHoly.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCHOLY_DIALOG              102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT1                       1000
+#define IDC_EDIT2                       1001
+#define IDC_BTN_NO                      1002
+#define IDC_BTN_NAME                    1003
+#define IDC_BTN_NO_CHK                  1004
+#define IDC_BTN_NAME_CHK                1005
+#define IDC_CG                          1006
+#define IDC_BTN_LIST                    1007
+#define IDC_LIST                        1008
+#define IDC_BTN_COMBO                   1009
+#define IDC_COMBO                       1010
+#define IDC_EDIT4                       1011
+#define IDC_CNT_LIST                    1011
+#define IDC_CNT_COMBO                   1012
+#define IDC_BTN_LIST_DEL                1013
+#define IDC_BTN_COMBO_DEL               1014
+#define IDC_CHK1                        1015
+#define IDC_CHK2                        1016
+#define IDC_EDIT_CHK1                   1017
+#define IDC_EDIT_CHK2                   1018
+#define IDC_RADIO1                      1019
+#define IDC_RADIO2                      1020
+#define IDC_RADIO3                      1021
+#define IDC_EDIT3                       1022
+#define IDC_ST_RA                       1022
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1023
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
